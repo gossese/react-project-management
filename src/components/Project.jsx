b28b65f7f36288549ab2f6a708416e137c2e0c4c
Emma Gosses <emma@macbookairM4>
2025-12-27 17:56:23 +0100

    mostly styling updates after instructions

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,14 +3,15 @@ import TaskOverview from "./Task/TaskOverview";
 export default function Project({deleteProject, project, handleDeleteTask, handleAddTask, taskList}){
 
     return (
-        <article className="w-[35rem] mt-24">
-            <div className="pb-2 flex justify-between capitalize">
-                <h2 className="text-stone-600 text-3xl font-semibold ">{project.title}</h2>
-                <button onClick={() => deleteProject(project)}>Delete</button>
-            </div>
-            <time className="pb-2 text-stone-400 font-light text-sm" dateTime={project.date}>{project.date}</time>
-            <p className="py-2 text-stone-800">{project.description}</p>
-            <hr className="decoration-gray-500 h-6"></hr>
+        <article className="w-[35rem] mt-16">
+            <header border-b-2>
+                <div className="pb-4 mb-4 flex border-b-2 justify-between capitalize">
+                    <h1 className="text-stone-600 text-3xl font-semibold ">{project.title}</h1>
+                    <button onClick={() => deleteProject(project)}>Delete</button>
+                </div>
+                <p className="mb-4 text-stone-400 font-light text-sm" dateTime={project.date}>{project.date}</p>
+                <p className="my-4 text-stone-800 whitespace-pre-wrap">{project.description}</p>
+            </header>
             <TaskOverview project={project} handleAddTask={handleAddTask} handleDeleteTask={handleDeleteTask} taskList={taskList}></TaskOverview>
         </article>
     )
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,16 +1,27 @@
-import PrimaryButton from "./primaryButton"
+import PrimaryButton from "./primaryButton";
 
-export default function ProjectSidebar({projects, handleClick, handleClickProject}) {
-
-    return (
-        <aside className="bg-stone-900 text-stone-50 w-1/3 px-8 py-16 flex flex-col mt-12 rounded-r-xl tracking-wide md:w-72" >
-            <h2 className="mb-8 uppercase font-semibold md:text-xl">Your Projects</h2>
-            <PrimaryButton onClick={handleClick}>+ Add Project</PrimaryButton>
-            {<ul className="mt-8">
-                {projects.map((project, index) => {
-                    return <li className="my-1 p-1 text-stone-400 text-md capitalize font-light rounded hover:text-stone-200 hover:bg-stone-900 hover:cursor-pointer" key={index} onClick={() => handleClickProject(project)}>{project.title}</li>
-                })}
-            </ul>}
-        </aside>
-    )
+export default function ProjectSidebar({
+  projects,
+  handleClick,
+  handleClickProject,
+}) {
+  return (
+    <aside className="bg-stone-900 text-stone-50 w-1/3 px-8 py-16 flex flex-col mt-12 rounded-r-xl tracking-wide md:w-72">
+      <h2 className="mb-8 uppercase font-semibold md:text-xl">Your Projects</h2>
+      <PrimaryButton onClick={handleClick}>+ Add Project</PrimaryButton>
+        <ul className="mt-8">
+          {projects.map((project) => {
+            return (
+              <li
+                className="my-1 p-1 text-stone-400 text-md capitalize font-light rounded hover:text-stone-200 hover:bg-stone-900 hover:cursor-pointer"
+                key={project.id}
+                onClick={() => handleClickProject(project)}
+              >
+                {project.title}
+              </li>
+            );
+          })}
+        </ul>
+    </aside>
+  );
 }
